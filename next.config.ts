4d9e4c1d9064a478938d96448c3556250e55a31d
1209855574 <1209855574@qq.com>
2026-03-31 15:37:54 +0800

    fix: allow WeRead CDN image domains in next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['better-sqlite3'],
+  images: {
+    remotePatterns: [
+      { hostname: 'cdn.weread.qq.com' },
+      { hostname: 'weread-1258476243.file.myqcloud.com' },
+      { hostname: 'rescdn.qqmail.com' },
+    ],
+  },
 };
 
 export default nextConfig;
